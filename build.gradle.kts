b3f4c264f7ce420a38b28d5d4d6c1a5a0190d713
lloydnas <madry0404@yahoo.com>
2023-04-06 20:46:29 -0600

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    kotlin("multiplatform") version "1.7.10"
+    id("com.android.application")
+    id("kotlin-android-extensions")
+}
+
+group = "me.lloydna"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    
+    iosArm64("iOS") {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    android()
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+        val iOSMain by getting
+        val iOSTest by getting
+        val androidMain by getting {
+            dependencies {
+                implementation("com.google.android.material:material:1.5.0")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+            }
+        }
+    }
+}
+
+android {
+    compileSdkVersion(31)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        applicationId = "me.lloydna.library"
+        minSdkVersion(24)
+        targetSdkVersion(31)
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
