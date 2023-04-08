2399b6fd8185d81bea2076d1025e25ecc8ec662f
lloydnas <madry0404@yahoo.com>
2023-04-08 02:42:40 -0600

    Publish to maven local and tests now working!

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,8 +69,8 @@ android {
         targetSdkVersion(31)
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     testOptions {
         unitTests {
